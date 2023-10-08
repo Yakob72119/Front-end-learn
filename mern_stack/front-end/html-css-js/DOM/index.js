45ba3fff6fad29a7b7b025e99b17c6fc29541b08
Yakob72119 <beshayakob51@gmail.com>
2023-10-08 21:59:36 +0300

    still learning

---
+++ mern_stack/front-end/html-css-js/DOM/index.js
@@ -0,0 +1,8 @@
+// document.querySelector("h1").innerHTML = "Player one win";
+// document.querySelector("input[type=checkbox]").checked = "";
+// alert("Player one win");
+var num1= Math.floor(Math.random()*6) + 1;
+var num2= Math.floor(Math.random()*6) + 1;
+if(num1 === 1){
+   
+}
