dfc1642f5f9a5657025aa8056bd5e5e784378918
Yakob Beshahwered <beshayakob72119@gmail.com>
2024-04-10 15:01:37 +0300

    passport js done

--- mern_stack/front-end/Secrets - Starting Code/app.js
+++ mern_stack/front-end/Secrets - Starting Code/app.js
@@ -4,7 +4,15 @@ const express= require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose=require("mongoose");
-const encrypt =require("mongoose-encryption");
+// for passport
+const session = require('express-session');
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose"); 
+// const encrypt =require("mongoose-encryption"); lelvel 2
+// const md5=require("md5"); level 3
+
+// const bcrypt= require("bcrypt"); level 5
+//const saltRounds =10;
 const app= express();
 
 app.use(express.static("public"));
@@ -13,16 +21,39 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+ /// setup our session
+
+app.use(session({
+    secret:"our little secrete.",
+    resave: false,
+    saveUninitialized:false
+}))
+
+//  initalizing passport
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 mongoose.connect("mongodb://localhost:27017/userDB")
 const userSchema= new mongoose.Schema({
     email:String,
     password: String
 });
 
-///here it was the secrete
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
+userSchema.plugin(passportLocalMongoose); // to hash and salt our password
 
-const User=new mongoose.model("User", userSchema)
+///here it was the secrete
+//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});
+
+const User=new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.get("/", (req, res)=>{
     res.render("home")
 })
@@ -37,34 +68,114 @@ app.get("/register", (req, res)=>{
     res.render("register")
 })
 
-app.post("/register", (req,res)=>{
-    const {username, password} =req.body;
-    const newUser= new User({
-        email: username,
-        password: password
-    })
-    newUser.save().then(result => {
-        res.render("secrets")
-      })
-      .catch(err => {
-        console.log(err)
-      });
+app.get("/secrets", (req,res)=>{
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login")
+    }
 });
+app.get("/logout", (req,res)=>{
+    req.logout();
+    res.redirect("/");
+})
+app.post("/register", (req,res)=>{  
+   
+    // this register method comes from passport local mongoose  and it readuse new user= things
+    User.register({username:req.body.username}, req.body.password, function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/secrets");
+        }else{
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+
+});
+
+
 
 app.post("/login",(req,res)=>{
-    const {username, password}=req.body;
-
-    User.findOne({email:username}).then(foundUser=>{
-        if(foundUser){
-            if(foundUser.password === password){
-               res.render("secrets");
-            }
-        }
-    }).catch(err=>{
-        console.log(err)
-    });
+     const user =new User({
+      username:req.body.username,
+      password:req.body.password  
+     })   
     
+     req.login(user, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/secrets");
+            });
+        }
+     })
+
 });
+
+
 app.listen(3000, function(){
     console.log("server statrted on port 3000")
 })
+
+
+
+// app.post("/register", (req,res)=>{  
+    //               for bcrypt
+    // const {username, password} =req.body;
+    // bcrypt.hash(password, saltRounds, function(err, hash) {
+    //     const newUser= new User({
+    //         email: username,
+    //         password:hash
+    //     })
+    //     newUser.save().then(result => {
+    //         res.render("secrets")
+    //       })
+    //       .catch(err => {
+    //         console.log(err)
+    //       });
+    // });
+
+    //                     for md5 this is how it should be performed
+    // const newUser= new User({
+    //     email: username,
+    //     // just use md5 hash fection which is ireversable
+    //     password: md5(password)
+    // })
+    // newUser.save().then(result => {
+    //     res.render("secrets")
+    //   })
+    //   .catch(err => {
+    //     console.log(err)
+    //   });
+// });
+
+
+
+// app.post("/login",(req,res)=>{
+    //     const username=req.body.username;
+    //    // const password=md5(req.body.password;
+    //    const password=req.body.password;
+
+    //     User.findOne({email:username}).then(foundUser=>{
+    //         if(foundUser){
+
+    //                     // for md5
+    //             // if(foundUser.password === password){
+    //             //    res.render("secrets");
+    //             // }
+
+    //             bcrypt.compare(password, foundUser.password, function(err, result) {
+    //                 if(result === true){
+    //                     res.render("secrets");
+    //                 }
+
+    //             });
+    //         }
+    //     }).catch(err=>{
+    //         console.log(err)
+    //     });
+
+// });
